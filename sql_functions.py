cd12d97efd6d540c789f3fc330570c746022700a
Lavhran <lavhran@gmail.com>
2021-10-26 12:15:50 +0200

    Fix-5
    Same as problem that was fixed in Fix-3, but this one was in the
    temporary password creator.

--- sql_functions.py
+++ sql_functions.py
@@ -81,8 +81,7 @@ def isAdministrator(id: str) -> bool:
 def create_temporary_password(name: str, adminid: str) -> str:
     from random import choice as ranchoice
 
-    chars = "1£2£3£4£5£6£7£8£9£0£q£w£e£r£t£y£u£i£o£p£a£s£d£f£g£h£j£k£l£z£x£c£v£b£n£m£Q£W£E£R£T£Y£U£I£O£P£A£S£D£F£G£H£J£K£L£Z£X£C£V£B£N£M£-£_£,£.£;£:£<£>£#£&£!£?".split(
-        '£')
+    chars = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM-_,.;:<>#&!?"
 
     temppass = []
     for i in range(20):
